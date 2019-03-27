d7fd539703e741e76332f7f7ad0b686994431d65
michal@vantage <cz@signicode.com>
2019-03-27 15:04:55 +0100

    Clean up and DRY

--- test/unit/test.js
+++ test/unit/test.js
@@ -2,7 +2,7 @@ const {python} = require("../../");
 
 module.exports = {
     async test(test) {
-        test.expect(1);
+        test.expect(2);
 
         const x = {a:1, b: 2};
         const func = python`
@@ -11,8 +11,7 @@ module.exports = {
         `;
 
         test.equals(typeof func, "function", "Returns a function");
-        const data = await func(1);
-        console.log(data);
+        test.equals(await func(1), 4, "Does execute the function");
 
         test.done();
     }
